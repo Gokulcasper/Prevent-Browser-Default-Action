08cd74ff7a8902a34d3939a739e09d1e81921448
Gokulcasper <ronitcasper215@gmail.com>
2022-06-30 13:59:21 +0530

    Feat : Add Default Action

--- script2.js
+++ script2.js
@@ -2,23 +2,19 @@ const formEl = document.querySelector("form");
 const inputEl = document.querySelector("form input");
 const checkboxEl = document.querySelector("form input[type='checkbox']");
 
-formEl.addEventListener(
-  "submit",
-  (event) => {
-    event.preventDefault(); // Stops defaults actions
+formEl.addEventListener("submit", (event) => {
+  event.preventDefault(); // Stops defaults actions
 
-    if (inputEl.value.length <= 3 || inputEl.value.length > 20) {
-      alert("Validation error : Enter Name 3 to 20 Letters");
-      return;
-    }
-
-    if (!checkboxEl.checked) {
-      alert("check the Terms");
-      return;
-    }
-    console.log("Form Submitted : ", inputEl.value, checkboxEl.checked);
-
-    console.log("Default Prevent : ", event.defaultPrevented);
+  if (inputEl.value.length <= 3 || inputEl.value.length > 20) {
+    alert("Validation error : Enter Name 3 to 20 Letters");
+    return;
   }
-  //   { passive: true }
-);
+
+  if (!checkboxEl.checked) {
+    alert("check the Terms");
+    return;
+  }
+  console.log("Form Submitted : ", inputEl.value, checkboxEl.checked);
+
+  console.log("Default Prevent : ", event.defaultPrevented);
+});
